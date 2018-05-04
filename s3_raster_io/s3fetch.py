ceee2ac56c76c6237e2cf319c8970d33fb63db6e
Kirill Kouzoubov <kirill888@gmail.com>
2018-05-04 16:08:04 +1000

    Add method for generating signed s3 get_object requests

--- s3_raster_io/s3fetch.py
+++ s3_raster_io/s3fetch.py
@@ -31,11 +31,13 @@ def get_s3_client(region_name='ap-southeast-2',
     return s3
 
 
-def s3_ls(url):
+def s3_url_parse(url):
     uu = urlparse(url)
+    return uu.netloc, uu.path.lstrip('/')
 
-    bucket = uu.netloc
-    prefix = uu.path.lstrip('/')
+
+def s3_ls(url):
+    bucket, prefix = s3_url_parse(url)
 
     s3 = get_s3_client()
     paginator = s3.get_paginator('list_objects')
@@ -148,6 +150,59 @@ def tif_read_tile(url, tile_idx, hdr_max_sz=4096, s3=None, dtype=None):
                             chunk_size=nbytes)
 
     return hdr, im, stats
+
+
+def s3_get_object_request_maker(region_name=None, credentials=None, ssl=True):
+    from botocore.session import get_session
+    from botocore.auth import S3SigV4Auth
+    from botocore.awsrequest import AWSRequest
+    from urllib.request import Request
+
+    session = get_session()
+
+    if region_name is None:
+        region_name = session.get_config_variable('region')
+
+    if region_name is None:
+        raise ValueError('Region name is not supplied and no default is configured')
+
+    if credentials is None:
+        credentials = session.get_credentials().get_frozen_credentials()
+
+    protocol = 'https' if ssl else 'http'
+    auth = S3SigV4Auth(credentials, 's3', region_name)
+
+    def build_request(bucket=None,
+                      key=None,
+                      url=None,
+                      Range=None):
+        if key is None and url is None:
+            if bucket is None:
+                raise ValueError('Have to supply bucket,key or url')
+            # assume bucket is url
+            url = bucket
+
+        if url is not None:
+            bucket, key = s3_url_parse(url)
+
+        if isinstance(Range, (tuple, list)):
+            Range = 'bytes={}-{}'.format(Range[0], Range[1]-1)
+
+        headers = {}
+        if Range is not None:
+            headers['Range'] = Range
+
+        req = AWSRequest(method='GET',
+                         url='{}://s3.{}.amazonaws.com/{}/{}'.format(protocol, region_name, bucket, key),
+                         headers=headers)
+
+        auth.add_auth(req)
+
+        return Request(req.url,
+                       headers=dict(**req.headers),
+                       method='GET')
+
+    return build_request
 
 
 class S3TiffReader(object):
