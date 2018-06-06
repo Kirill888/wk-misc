69b9be8e0631ae0a0e5dc364580d5cee71d281af
Kirill Kouzoubov <kirill888@gmail.com>
2018-06-06 12:09:00 +1000

    Add option to process all products

--- dc-tools/dc_tools/app.py
+++ dc-tools/dc_tools/app.py
@@ -5,25 +5,49 @@ import random
 import sys
 from urllib.parse import urlparse
 
+
 def random_string(prefix):
     return '{}{:04x}'.format(prefix, random.getrandbits(16))
+
+
+def get_managed_products(db):
+    c = db.cursor()
+    c.execute('''SELECT
+dataset_type.id AS id,
+dataset_type.name AS product,
+(dataset_type.definition #>> '{storage,tile_size,x}')::float AS tx,
+(dataset_type.definition #>> '{storage,tile_size,y}')::float AS ty
+
+FROM agdc.dataset_type
+WHERE
+    (dataset_type.definition #>> '{managed}')::bool AND
+    (dataset_type.definition -> 'storage' ? 'tile_size');
+''')
+
+    products = {}
+
+    for (pid, name, tx, ty) in c.fetchall():
+        products[name] = SimpleNamespace(name=name, id=pid, tx=tx, ty=ty)
+
+    c.close()
+    return products
 
 
 def mk_grouper():
     dd = {}
 
-    def mk_dataset(uuid, loc_id, t_in, t_out):
-        return SimpleNamespace(uuid=uuid, loc_id=loc_id, time=(t_in, t_out))
+    def mk_dataset(uuid, loc_id, t_in, t_out, uri):
+        return SimpleNamespace(uuid=uuid, loc_id=loc_id, time=(t_in, t_out), uri=uri)
 
     def _do_group(uri, lx, ly, t_in, t_out, uuid, loc_id, product):
-        uri, *_ = uri.split('#')
-        ds = mk_dataset(uuid, loc_id, t_in, t_out)
-        f = dd.get(uri)
+        uri_base, *_ = uri.split('#')
+        ds = mk_dataset(uuid, loc_id, t_in, t_out, uri)
+        f = dd.get(uri_base)
         if f is None:
-            dd[uri] = SimpleNamespace(uri=uri,
-                                      cell=(lx, ly),
-                                      product=product,
-                                      datasets=[ds])
+            dd[uri_base] = SimpleNamespace(uri=uri_base,
+                                           cell=(lx, ly),
+                                           product=product,
+                                           datasets=[ds])
         else:
             assert f.cell == (lx, ly)
             f.datasets.append(ds)
@@ -34,6 +58,30 @@ def mk_grouper():
         return _do_group(*args)
 
     return proc
+
+
+def mk_location_query(db, p):
+    c = db.cursor(random_string('cursor'))
+
+    c.execute('''SELECT
+CONCAT( dataset_location.uri_scheme, ':', dataset_location.uri_body) AS uri,
+    ((dataset.metadata #>> '{grid_spatial, projection, geo_ref_points, ll, x}')::float/%(tx)s)::integer AS llx,
+    ((dataset.metadata #>> '{grid_spatial, projection, geo_ref_points, ll, y}')::float/%(ty)s)::integer AS lly,
+    (dataset.metadata #>> '{extent, from_dt}')::timestamp AS from_dt,
+    (dataset.metadata #>> '{extent, to_dt}')::timestamp AS to_dt,
+    dataset.id AS uuid,
+    dataset_location.id as loc_id,
+    %(product_name)s AS product
+
+FROM (agdc.dataset INNER JOIN
+      agdc.dataset_location ON dataset.id=dataset_location.dataset_ref)
+WHERE dataset.archived is NULL
+    AND dataset_location.archived is NULL
+    AND dataset.dataset_type_ref = %(product_id)s
+;
+''', dict(tx=p.tx, ty=p.ty, product_name=p.name, product_id=p.id))
+
+    return c
 
 
 cli = click.Group(name='dctools', help="")
@@ -64,50 +112,40 @@ def files(host, port, user, dbname, product):
         host = None
         port = None
 
+    def process_one(db, prod):
+        c = mk_location_query(db, prod)
+        proc = mk_grouper()
+
+        for idx, v in enumerate(c):
+            proc(*v)
+            if (idx % 10000) == 0:
+                sys.stderr.write('.')
+                sys.stderr.flush()
+
+        for f in proc():
+            uri = urlparse(f.uri)
+            if uri.scheme == 'file':
+                uri = uri.path
+            else:
+                uri = f.uri
+
+            print('{f.product:>16s} {f.cell[0]:3d} {f.cell[1]:3d} {n:4d} {uri}'.format(f=f, uri=uri, n=len(f.datasets)))
+
     db = psycopg2.connect(host=host, port=port, user=user, dbname=dbname)
 
-    c = db.cursor(random_string('cursor'))
+    prods = get_managed_products(db)
 
-    c.execute('''SELECT
-CONCAT(dataset_location.uri_scheme, ':', dataset_location.uri_body) AS uri,
-((dataset.metadata #>> '{grid_spatial, projection, geo_ref_points, ll, x}')::float/product.tx)::integer AS llx,
-((dataset.metadata #>> '{grid_spatial, projection, geo_ref_points, ll, y}')::float/product.ty)::integer AS lly,
-(dataset.metadata #>> '{extent, from_dt}')::timestamp AS from_dt,
-(dataset.metadata #>> '{extent, to_dt}')::timestamp AS to_dt,
-dataset.id AS uuid,
-dataset_location.id as loc_id,
-product.name AS product
-
-FROM ((agdc.dataset INNER JOIN
-     (SELECT id,
-        name,
-        (definition #>> '{storage,tile_size,x}')::float AS tx,
-        (definition #>> '{storage,tile_size,y}')::float AS ty
-        FROM agdc.dataset_type
-        WHERE name=%(product)s) AS product
-      ON product.id = dataset.dataset_type_ref)
-INNER JOIN agdc.dataset_location ON dataset.id=dataset_location.dataset_ref)
-WHERE dataset.archived is NULL
-AND dataset_location.archived is NULL
-;
-''', dict(product=product))
-
-    proc = mk_grouper()
-
-    for idx, v in enumerate(c):
-        proc(*v)
-        if (idx % 10000) == 0:
-            sys.stderr.write('.')
+    if product == '*all*':
+        for prod in prods.values():
+            sys.stderr.write('\n{}\n'.format(prod.name))
+            process_one(db, prod)
             sys.stderr.flush()
+    else:
+        if product not in prods:
+            click.echo('Product {} is not in a database or is not a managed product'.format(product), err=True)
+            sys.exit(1)
 
-    for f in proc():
-        uri = urlparse(f.uri)
-        if uri.scheme == 'file':
-            uri = uri.path
-        else:
-            uri = f.uri
-
-        print('{f.product:>16s} {f.cell[0]:3d} {f.cell[1]:3d} {n:4d} {uri}'.format(f=f, uri=uri, n=len(f.datasets)))
+        process_one(db, prods[product])
 
 
 ####################################################################
@@ -120,3 +158,19 @@ def test_random_string():
     assert s1 != s2
     assert s1.startswith('kk')
     assert s2.startswith('kk')
+
+
+def test_get_managed_product():
+    db = psycopg2.connect(dbname='kk2')
+    print(get_managed_products(db))
+
+
+def test_loc_query():
+    db = psycopg2.connect(dbname='kk2')
+    pp = get_managed_products(db)
+
+    c = mk_location_query(db, pp['ls8_nbar_albers'])
+    print(c.query.decode('ascii'))
+
+    for x in c:
+        print(x)
