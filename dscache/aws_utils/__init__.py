aa086c3e333b44ebf443b4704ceaf9198ef91140
Kirill Kouzoubov <kirill888@gmail.com>
2018-08-14 17:24:09 +1000

    Copied slurp_lines to new sub-project

--- dscache/aws_utils/__init__.py
+++ dscache/aws_utils/__init__.py
@@ -1,2 +1,10 @@
 """
 """
+
+
+def slurp_lines(fname, *args, **kwargs):
+    if len(args) > 0 or len(kwargs) > 0:
+        fname = fname.format(*args, **kwargs)
+
+    with open(fname, 'rt') as f:
+        return [s.rstrip() for s in f.readlines()]
