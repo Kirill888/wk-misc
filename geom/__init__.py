e22bbb827a3ce44be65cf384272e72721d8101cd
Kirill Kouzoubov <kirill888@gmail.com>
2018-09-20 20:49:09 +1000

    Better (proper this time) algorithm for Affine decomposition

--- geom/__init__.py
+++ geom/__init__.py
@@ -4,10 +4,10 @@ from affine import Affine
 def decompose_rws(A):
     """Compute decomposition Affine matrix sans translation into Rotation Shear and Scale.
 
-    Note: only works for smallish values of shear, separation is almost exact
-    when shear is 0. Note that there are ambiguities for negative scales.
+    Note: that there are ambiguities for negative scales.
 
-    Example: R(90)*S(1,1) == R(-90)*S(-1,-1)
+    Example: R(90)*S(1,1) == R(-90)*S(-1,-1),
+    (R*(-I))*((-I)*S) == R*S
 
     A = R W S
 
@@ -18,7 +18,7 @@ def decompose_rws(A):
 
     """
     from numpy import diag, asarray
-    from numpy.linalg import svd, det
+    from numpy.linalg import cholesky, det, inv
 
     if isinstance(A, Affine):
         def to_affine(m, t=(0, 0)):
@@ -37,30 +37,16 @@ def decompose_rws(A):
 
     assert A.shape == (2, 2)
 
-    u, s, vh = svd(A)
+    WS = cholesky(A.T @ A).T
+    R = A @ inv(WS)
 
-    rr = u @ vh
-    if det(rr) < 0:
-        rr[:, -1] *= -1
+    if det(R) < 0:
+        R[:, -1] *= -1
+        WS[-1, :] *= -1
 
-    ss = diag(rr.T @ A)  # got the scale in `ss`
-
-    RW = A @ diag(1.0/ss)  # remove scale from A: `R W S inv(S) = A inv(S)`
-
-    # RW is:
-    #         cos(a), w*cos(a) - sin(a)
-    #         sin(a), w*sin(a) + cos(a)
-
-    ca, sa = RW[:, 0]
-
-    R = asarray([[ca, -sa],
-                 [sa, ca]])
-
-    w = (R.T @ RW)[0, 1]  # R.T R W = W
-
-    W = asarray([[1, w],
-                 [0, 1]])
+    ss = diag(WS)
     S = diag(ss)
+    W = WS @ diag(1.0/ss)
 
     return R, W, S
 
@@ -99,14 +85,14 @@ def test_rsw():
     def get_diff(A, B):
         return sqrt(sum((a-b)**2 for a, b in zip(A, B)))
 
-    def run_test(a, scale, tol=1e-6):
-        A = mkA(a, scale=scale)
+    def run_test(a, scale, shear=0, translation=(0, 0), tol=1e-8):
+        A = mkA(a, scale=scale, shear=shear, translation=translation)
 
         R, W, S = decompose_rws(A)
 
         assert get_diff(A, R*W*S) < tol
         assert get_diff(S, mkA(0, scale)) < tol
-        assert get_diff(R, mkA(a)) < tol
+        assert get_diff(R, mkA(a, translation=translation)) < tol
 
     for a in (0, 12, 45, 33, 67, 89, 90, 120, 170):
         run_test(a, (1, 1))
@@ -118,6 +104,9 @@ def test_rsw():
 
         run_test(a, (2, -1))
         run_test(-a, (2, -1))
+
+    run_test(0, (3, 4), 10)
+    run_test(-33, (3, -1), 10, translation=(100, -333))
 
 
 def test_fit():
